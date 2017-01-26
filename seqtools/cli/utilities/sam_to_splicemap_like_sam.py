c025e6abc728b3ac49aa9a3942bd57caee6580c0
Jason L Weirather <jason.weirather@gmail.com>
2017-01-26 15:34:51 -0600

    part 1 of making hisat2 compatible with IDP

---
+++ seqtools/cli/utilities/sam_to_splicemap_like_sam.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+"""Convert a BAM/SAM file into one that is compatible with applications
+that require a splicemap output SAM.  Requires samtools.
+
+This makes the IDP and IDP-fusion in versions that depend on splicemap
+to accept more universal inputs.
+
+"""
+import sys, argparse
+from seqtools.format.sam import SAMStream
+from subprocess import PIPE, Popen
+
+def main(args):
+  inf = sys.stdin
+  if args.input != '-':
+    cmd = 'samtools view -h '+args.input
+    pinf = Popen(cmd.split(),stdout=PIPE)
+    inf = pinf.stdout
+  of = sys.stdout
+
+  if args.output:
+    if args.output[-4:] == '.bam':
+      cmd = 'samtools view -Sb - -o '+args.output
+      pof = Popen(cmd.split(),stdin=PIPE)
+      of = pof.stdin
+    else: of = open(args.output,'w')
+
+  stream = SAMStream(inf)
+  of.write(stream.header_text)
+  z = 0
+  for sam in stream:
+    z+=1
+    if not sam.is_aligned(): continue # just keep aligned 
+    cigar = sam.get_cigar()
+    leftoffset = 0
+    leftcigaroffset = 0
+    if cigar[0][1] == 'H':
+      if cigar[1][1] == 'S':
+        leftoffset = cigar[1][0]
+        leftcigaroffset = 2
+    elif cigar[0][1] == 'S':
+      leftoffset = cigar[0][0]
+      leftcigaroffset = 1
+    rightoffset = 0
+    rightcigaroffset = 0
+    if cigar[-1][1] == 'H':
+      if cigar[-2][1] == 'S':
+        rightoffset = cigar[-2][0]
+        rightcigaroffset = 2
+    elif cigar[-1][1] == 'S':
+      rightoffset = cigar[-1][0]
+      rightcigaroffset = 1
+    origseq = sam.value('seq')
+    seq = '*'
+    if origseq != '*':
+      seq = origseq[leftoffset:len(origseq)-rightoffset]
+    qual = '*'
+    if sam.value('qual') != '*':
+      qual = sam.value('qual')[leftoffset:len(sam.value('qual'))-rightoffset]
+    neocigar = cigar[leftcigaroffset:len(cigar)-rightcigaroffset]
+    neocigarstring = ''.join([str(x[0])+x[1] for x in neocigar])
+    flag = "0"
+    if sam.check_flag(16):
+      flag = "16"
+    ostr = ''
+    ostr += sam.value('qname') + "\t"
+    ostr += flag + "\t"
+    ostr += sam.value('rname') + "\t"
+    ostr += str(sam.value('pos')) + "\t"
+    ostr +=  "0" + "\t"
+    ostr += neocigarstring + "\t"
+    ostr += '*' + "\t"
+    ostr += "0" + "\t"
+    ostr += str(sam.value('tlen')) + "\t"
+    ostr += seq + "\t"
+    ostr += qual
+    of.write(ostr+"\n")
+    if z%1000==0: sys.stderr.write("processed "+str(z)+" lines. At: "+sam.value('rname')+':'+str(sam.value('pos'))+"           \r")
+
+  sys.stderr.write("\n")
+  if args.input != '-':
+    pinf.communicate()
+  if args.output:
+    if args.output[-4:] == '.bam': pof.communicate()
+    else: of.close()
+
+def do_inputs():
+  # Setup command line inputs
+  parser=argparse.ArgumentParser(description="Generate our .bgi index for a bam file",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('input',help="Input BAM or - for STDIN SAM")
+  parser.add_argument('--output','-o',help="Specifiy path to output, will be bam format if '.bam' is specified, STDOUT sam if not set")
+  args = parser.parse_args()
+  return args
+
+def external_cmd(cmd):
+  cache_argv = sys.argv
+  sys.argv = cmd.split()
+  args = do_inputs()
+  main(args)
+  sys.argv = cache_argv
+
+if __name__=="__main__":
+  args = do_inputs()
+  main(args)
