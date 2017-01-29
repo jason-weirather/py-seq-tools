036f1a82edbfabf933399744af9afcdc0ef9230e
Jason Weirather <jason.weirather@gmail.com>
2017-01-29 08:53:32 -0600

    annotate two componts of simulation

--- seqtools/simulation/emitter.py
+++ seqtools/simulation/emitter.py
@@ -1,8 +1,17 @@
+"""These classes should produce simulation products"""
 from seqtools.simulation.randomsource import RandomSource
 
-#Give it a transcriptome definition and a reference genome for it
-#initialy give it uniform probability
 class TranscriptomeEmitter:
+  """Give it a transcriptome definition and a reference genome for it
+     initialy give it uniform probability
+
+  :param transcriptome: A transcriptome from which to produce transcripts
+  :param seed: Seeded random generation
+  :param rand: A class that can generate randome numbers if you have one already seeded or want totally random
+  :type transcriptome: Transcriptome
+  :type seed: int
+  :type rand: RandomSource
+  """
   def __init__(self,transcriptome,seed=None,rand=None):
     if rand: self.random = rand
     elif seed: self.random = RandomSource(seed)
@@ -16,11 +25,21 @@ class TranscriptomeEmitter:
     self._log = []
 
   def emit_transcript(self):
+    """Get a transcript according according to weight of transcript
+
+    :return: One random Transcript
+    :rtype: Transcript
+    """
     i = self.random.get_weighted_random_index(self._weights)
     return self._transcriptome.get_transcripts()[i]
 
-  # input: an array of weights <<txname1> <weight1>> <<txname2> <weight2>>...
   def set_weights_by_dict(self,weights):
+    """input: an array of weights <<txname1> <weight1>> <<txname2> <weight2>>...
+       if this does not get set then even weighting will be used
+
+    :param weights: [[tx1,wght1],[tx2,wght2],...[txN,wightN]]
+    :type weights: list
+    """
     self._weights = []
     txnames = [x.get_transcript_name() for x in self._transcriptome.get_transcripts()]
     for txname in txnames:
--- seqtools/simulation/permute.py
+++ seqtools/simulation/permute.py
@@ -1,9 +1,11 @@
+"""These classes are here to alter simulation products or other sequences"""
 import math
 from seqtools.sequence import rc
 from seqtools.simulation.randomsource import RandomSource
 from seqtools.format.fastq import FASTQ
 
 class MakeErrors:
+  """Class to define how to make errors, and to introduce those errors"""
   def __init__(self,rand=None,seed=None):
     if rand:
       self.random = rand
@@ -19,15 +21,28 @@ class MakeErrors:
     self._modified_base = None
 
   def set_before_context(self,base):
+    """Limit errors to a specific preceeding base context"""
     self._before_base = base
   def set_after_context(self,base):
+    """Limit errors to a specific following base context"""
     self._after_base = base
   def set_observed_base(self,base):
+    """Limit errors to a specific reference base"""
     self._observed_base = base
   def set_modified_base(self,base):
+    """Limit errors to a specific type of sequenced base"""
     self._modified_base = base
 
   def random_substitution(self,fastq,rate):
+    """Perform the permutation on the sequence
+
+    :param fastq: FASTQ sequence to permute
+    :type fastq: FASTQ
+    :param rate: how frequently to permute
+    :type rate: float
+    :return: Permutted FASTQ
+    :rtype: FASTQ
+    """
     sequence = fastq.seq
     seq = ''
     for i in range(len(sequence)):
@@ -57,6 +72,15 @@ class MakeErrors:
     return FASTQ('@'+fastq.header+"\n"+seq+"\n+\n"+fastq.qual+"\n")
 
   def random_deletion(self,fastq,rate):
+    """Perform the permutation on the sequence
+
+    :param fastq: FASTQ sequence to permute
+    :type fastq: FASTQ
+    :param rate: how frequently to permute
+    :type rate: float
+    :return: Permutted FASTQ
+    :rtype: FASTQ
+    """
     sequence = fastq.seq
     quality = fastq.qual
     seq = ''
@@ -88,6 +112,17 @@ class MakeErrors:
     return FASTQ('@'+fastq.header+"\n"+seq+"\n+\n"+qual+"\n")
 
   def random_insertion(self,rate,max_inserts=1):
+    """Perform the permutation on the sequence. If authorized to do multiple bases they are done at hte rate defined here.
+
+    :param fastq: FASTQ sequence to permute
+    :type fastq: FASTQ
+    :param rate: how frequently to permute
+    :type rate: float
+    :param max_inserts: the maximum number of bases to insert (default 1)
+    :type rate: int
+    :return: Permutted FASTQ
+    :rtype: FASTQ
+    """
     sequence = fastq.seq
     quality = fastq.qual
     seq = ''
@@ -135,11 +170,19 @@ class MakeErrors:
     return FASTQ('@'+fastq.name+"\n"+seq+"\n+\n"+qual+"\n")
 
   def random_flip(self,sequence):
+    """Change the direction of the sequence with 0.5 probability"""
     if self.random.random() < 0.5:
       return rc(sequence)
     return sequence
 
 class MakeCuts:
+  """Class to cut the sequence to different sizes
+
+  :param rand: pass a random source, otherwise it gets a new RandomSource
+  :param seed: if you want to set a seed here
+  :type rand: RandomSource
+  :type seed: int
+  """
   def __init__(self,rand=None,seed=None):
     if rand:
       self.random = rand
@@ -163,6 +206,8 @@ class MakeCuts:
     return seq[start:start+l]
 
   def set_custom(self,gmin,gmu,gsigma):
+    """Set a minimum lengtha, and then the gaussian distribution parameters for cutting
+       For any sequence longer than the minimum the guassian parameters will be used"""
     self._gauss_min = gmin
     self._gauss_mu = gmu
     self._gauss_sigma = gsigma
@@ -176,6 +221,7 @@ class MakeCuts:
     self._gauss_sigma = 290
 
 def random_flip(sequence,rnum=None):
+  """Flip a sequence direction with 0.5 probability"""
   randin = rnum
   if not randin: randin = RandomSource()
   if randin.random() < 0.5:
@@ -183,6 +229,9 @@ def random_flip(sequence,rnum=None):
   return sequence
 
 def rate_to_phred33(rate):
+  """Convert an error rate to a phred 33 character"""
   return chr(int(-10*math.log10(rate))+33)
+
 def phred33_to_rate(q):
+  """Convert a phred33 character to an error rate"""
   return math.pow(10,float(ord(q)-33)/-10)
