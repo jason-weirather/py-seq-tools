a30d705718bf4bba5e480ef07dc92a0ea0e9bcea
jason-weirather <jason-weirather@users.noreply.github.com>
2017-08-12 17:47:26 -0400

    update problems in readme

--- setup.py
+++ setup.py
@@ -7,14 +7,62 @@ if sys.version_info < (2,7):
    sys.exit("Error: You are using Python "+str(sys.version_info)+"; Python 2.6 and below are not supported. Please use 2.7 or better\n")
 
 this_folder = path.abspath(path.dirname(__file__))
-with open(path.join(this_folder,'README.md'),encoding='utf-8') as inf:
-  long_description = inf.read()
 
 setup(
   name='seq-tools',
-  version='1.0.0',
+  version='1.0.1',
   description='Python tools for working with biological sequence data',
-  long_description=long_description,
+  long_description='''
+# py-seq-tools
+Free python libraries for working with biological sequences, alignments, and formats. These libraries are part of Au-public scripts, I haven't documented them well yet.  The goal here is to put them into a package and get them better documented.
+
+Longer term, these were written in python 2, but it would be nice to either support python3 full compatability or to completely migrate them over to python3.
+
+The command line utilities of this package serve a dual purpose. 1) is to provide useful utilities you can run through the commandline, and 2) is to provide examples on how to make use the of the libraries included in this package.
+
+Access to the command line utilities can be found with
+
+`$ seq-tools <cli command>`
+
+Where cli command is the name of a cli module.
+
+To use the libraries in this distribution please consult the [API documentation](https://github.com/jason-weirather/py-seq-tools/blob/master/manual.pdf)
+
+You can install seq-tools with pip
+
+From within the source directory you can try
+
+`$ pip install .`
+
+This should make the `seq-tools` command available as well as the `seqtools` library for import.
+
+This library includes the following modules:
+
+* seqtools
+  * align
+  * errors
+  * format
+    * bamindex
+    * bed
+    * bgzf
+    * fasta
+    * fastq
+    * pacbio
+    * psl
+    * sam
+  * graph
+  * range
+  * sequence
+  * simulation
+    * emitter
+    * permute
+    * randomsource
+  * statistics
+  * stream
+  * structure
+
+Again, details are available in the [API documentation](https://github.com/jason-weirather/py-seq-tools/blob/master/manual.pdf).
+  ''',
   url='https://github.com/jason-weirather/py-seq-tools',
   author='Jason L Weirather',
   author_email='jason.weirather@gmail.com',
