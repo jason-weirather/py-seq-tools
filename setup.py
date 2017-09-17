e27de4550008b8d0c88adaeee77030a5bebf5ab8
jason-weirather <jason-weirather@users.noreply.github.com>
2017-09-17 19:07:40 -0400

    tick up version with fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_folder,'README.md'),encoding='utf-8') as inf:
 
 setup(
   name='seq-tools',
-  version='1.0.7',
+  version='1.0.8',
   description='Python tools for working with biological sequence data',
   long_description=long_description,
   url='https://github.com/jason-weirather/py-seq-tools',
